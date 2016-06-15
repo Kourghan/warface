1964f33f60fa024ef03b0d546a5a4b1981086141
Mikhaylo Tymoshchenko <morbop@gmail.com>
2016-06-15 13:49:56 +0300

    hex types

--- Warface/Warface/Factories/HexFactory.swift
+++ Warface/Warface/Factories/HexFactory.swift
@@ -12,7 +12,30 @@ import SpriteKit
 class HexFactory {
 	
 	class func hexForType(type: HexType) -> CombatHex {
+		let hex: CombatHex
 		
+		switch type {
+		case .Water:
+			hex = waterHex()
+		case .Mud:
+			hex = mudHex()
+		case .Grass:
+			hex = grassHex()
+		}
+		
+		return hex
+	}
+	
+	private class func waterHex() -> CombatHex {
+		return CombatHex(type: .Water, name: "water", sprite: SKSpriteNode(imageNamed: "hex_water"))
+	}
+	
+	private class func mudHex() -> CombatHex {
+		return CombatHex(type: .Mud, name: "mud", sprite: SKSpriteNode(imageNamed: "hex_mud"))
+	}
+	
+	private class func grassHex() -> CombatHex {
+		return CombatHex(type: .Grass, name: "grass", sprite: SKSpriteNode(imageNamed: "hex_grass"))
 	}
 	
 }
